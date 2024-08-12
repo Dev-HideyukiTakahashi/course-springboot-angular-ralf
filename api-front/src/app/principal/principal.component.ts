f5e8131c33bc55ab85de13931f5c379fd1897132
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 04:30:45 -0300

    configurando renderização oculta

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -19,6 +19,9 @@ export class PrincipalComponent {
   // Variavel para visibilidade dos botões
   btnCadastro:boolean = true;
 
+  // Variavel para visibilidade da tabela
+  tabela:boolean = true;
+
   // JSON de clientes
   clientes:Cliente[] = [];
 
@@ -37,8 +40,29 @@ export class PrincipalComponent {
   cadastrar():void{
     this.servico.cadastrar(this.cliente)
     .subscribe(retorno => {
+
+      // Cadastrar o cliente no vetor
       this.clientes.push(retorno);
+
+      // Limpar formulário
+      this.cliente = new Cliente();
+
+      // Mensagem
+      alert("Cliente cadastrado com sucesso!");
     })
+  }
+
+  // Método para selecionar um cliente específico
+  selecionarCliente(posicao:number):void{
+
+    // Selecionar cliente no vetor
+    this.cliente = this.clientes[posicao];
+
+    // Visibilidade dos botões
+    this.btnCadastro = false;
+
+    // Visibilidade da tabela
+    this.tabela = false;
   }
 
   // Método de inicialização
