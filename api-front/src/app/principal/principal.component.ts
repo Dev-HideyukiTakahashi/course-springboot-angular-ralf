87f5594beed661575a5bd19977f97d46c9e2c607
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 02:12:02 -0300

    Ocultando botões

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
 
+  // Variavel para visibilidade dos botões
+  btnCadastro:boolean = true;
 }
