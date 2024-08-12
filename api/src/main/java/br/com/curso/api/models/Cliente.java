be0f5149cfcacec0813d485d4e6fdeae621ceb46
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 00:21:47 -0300

    Adicionando entitidade

--- api/src/main/java/br/com/curso/api/models/Cliente.java
+++ api/src/main/java/br/com/curso/api/models/Cliente.java
@@ -1,5 +1,24 @@
 package br.com.curso.api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_clientes")
+@Getter
+@Setter
 public class Cliente {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long codigo;
+  private String nome;
+  private Integer idade;
+  private String cidade;
 
 }
