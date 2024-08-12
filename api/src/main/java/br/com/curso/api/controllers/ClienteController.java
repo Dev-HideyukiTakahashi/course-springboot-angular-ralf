67acb635693aa755b8ec8b91fed14ca26d44f064
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 00:51:44 -0300

    Adicionando editar + deletar

--- api/src/main/java/br/com/curso/api/controllers/ClienteController.java
+++ api/src/main/java/br/com/curso/api/controllers/ClienteController.java
@@ -3,8 +3,11 @@ package br.com.curso.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +29,15 @@ public class ClienteController {
   public List<Cliente> selecionar() {
     return repository.findAll();
   }
+
+  @PutMapping("/")
+  public Cliente editar(@RequestBody Cliente cliente) {
+    return repository.save(cliente);
+  }
+
+  @DeleteMapping(path = "/{codigo}")
+  public void remover(@PathVariable Long codigo) {
+    repository.deleteById(codigo);
+  }
+
 }
