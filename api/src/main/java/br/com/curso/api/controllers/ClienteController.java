04ea93767dd66150edb58717e91bc452f62ee562
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 00:41:30 -0300

    Adicionando cadastrar + selecionar todos

--- api/src/main/java/br/com/curso/api/controllers/ClienteController.java
+++ api/src/main/java/br/com/curso/api/controllers/ClienteController.java
@@ -1,13 +1,29 @@
 package br.com.curso.api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.curso.api.models.Cliente;
+import br.com.curso.api.repositories.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
+  @Autowired
+  private ClienteRepository repository;
+
+  @PostMapping("/")
+  public Cliente cadastrar(@RequestBody Cliente cliente) {
+    return repository.save(cliente);
+  }
+
   @GetMapping("/")
-  public String teste() {
-    return "Hello World!";
+  public List<Cliente> selecionar() {
+    return repository.findAll();
   }
 }
