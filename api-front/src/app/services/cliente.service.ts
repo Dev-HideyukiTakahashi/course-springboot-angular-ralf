55d5a2e90fd1c59cc44381052ffd9c3d999b8c33
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 02:41:38 -0300

    metodo front para selecionar todos clientes

--- api-front/src/app/services/cliente.service.ts
+++ api-front/src/app/services/cliente.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../model/Cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,12 @@ export class ClienteService {
 
   //URL da api
   private url:string = "http://localhost:8080";
+
+  //Construtor
   constructor(private http:HttpClient) { }
+
+  // Método para selecionar todos os clientes
+  selecionar():Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+  }
 }
