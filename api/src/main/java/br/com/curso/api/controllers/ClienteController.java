733935c160570a03352a533ebf995a3200e0ae81
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 00:59:52 -0300

    Adicionando CORS

--- api/src/main/java/br/com/curso/api/controllers/ClienteController.java
+++ api/src/main/java/br/com/curso/api/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.curso.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.curso.api.models.Cliente;
 import br.com.curso.api.repositories.ClienteRepository;
 
 @RestController
+@CrossOrigin("*")
 public class ClienteController {
 
   @Autowired
