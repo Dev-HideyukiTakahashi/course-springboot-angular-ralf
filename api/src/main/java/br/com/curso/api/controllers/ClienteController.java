5262bd1424c15c5be9562dffc4838014007a779d
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 00:16:28 -0300

    Testando rotas

--- api/src/main/java/br/com/curso/api/controllers/ClienteController.java
+++ api/src/main/java/br/com/curso/api/controllers/ClienteController.java
@@ -1,5 +1,13 @@
 package br.com.curso.api.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClienteController {
 
+  @GetMapping("/")
+  public String teste() {
+    return "Hello World!";
+  }
 }
