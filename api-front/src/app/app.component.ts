4b5e3773d932d87940e1ebefffbf5efdb92718db
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 02:19:32 -0300

    Modulo forms e http

--- api-front/src/app/app.component.ts
+++ api-front/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrincipalComponent],
+  imports: [CommonModule, RouterOutlet, PrincipalComponent, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
