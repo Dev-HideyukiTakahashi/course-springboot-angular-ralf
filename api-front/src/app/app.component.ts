5b2258716373be5123d80007aa82e18d1cb756f1
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 01:38:34 -0300

    Criando componente principal e adicionando no app.components

--- api-front/src/app/app.component.ts
+++ api-front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
