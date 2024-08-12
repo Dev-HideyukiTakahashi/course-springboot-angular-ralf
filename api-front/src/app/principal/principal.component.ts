5705cd6c88ab5696659d72d24530481919b87ff8
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 02:47:25 -0300

    método para request e armazenamento dos clientes

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Cliente } from '../model/Cliente';
+import { ClienteService } from '../services/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -12,4 +14,18 @@ export class PrincipalComponent {
 
   // Variavel para visibilidade dos botões
   btnCadastro:boolean = true;
+
+  // JSON de clientes
+  clientes:Cliente[] = [];
+
+  // Construtor para acesso ao cliente service para acesso as métodos
+  constructor(private servico:ClienteService){
+
+  }
+
+  // Método para selecionar clientes
+  selecionar():void{
+    this.servico.selecionar()
+    .subscribe(retorno => this.clientes = retorno);
+  }
 }
