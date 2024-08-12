00d39706d7767e10d7be8dc240adf2e89d18aa33
unknown <dev.hideyukitakahashi@gmail.com>
2024-08-12 00:25:33 -0300

    Adicionando repository

--- api/src/main/java/br/com/curso/api/repositories/ClienteRepository.java
+++ api/src/main/java/br/com/curso/api/repositories/ClienteRepository.java
@@ -1,5 +1,11 @@
 package br.com.curso.api.repositories;
 
-public interface ClienteRepository {
-  
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.curso.api.models.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
 }
